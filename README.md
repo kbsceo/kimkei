e54714cc175e74f2ace65891161297ea43e11a69
donginl <me@leedi.me>
2016-02-16 00:28:21 +0900

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# kimkei
+김케이인포(kimkei.info) 애플리케이션이다.
+
+서버 언어로 **Node.js** 를 사용하며, **express**를 이용하여 서비스를 제공한다<br/>
+데이터베이스 엔진은 **MongoDB**, 드라이버로 **mongoose** 를 사용한다.
+
+----------
+
+#### To Install
+<pre>$ npm install</pre>
+
+----------
+
+#### To run the server
+<pre>$ npm start</pre>
+
+----------
---
+++ app.js
@@ -0,0 +1,45 @@
+// 전역변수를 설정합니다.
+global._status = require('./status');
+global.async = require('async');
+global.database = require('./lib/database');
+
+// 쓸만한 모듈들을 가져와봅시다!
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser');
+
+/* var notFound = require('./middleware/notFound');
+var otherError = require('./middleware/otherError');
+var auth = require('./middleware/auth'); */
+
+// Express 생성
+var app = express();
+
+// View 엔진으로 ejs 를 씁시다~
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// 미들웨어들을 가져옵시다.
+// 쿠키랑 Request Body 를 읽을 모듈들을 가져옵시다.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(log.request());
+
+/* // 뭐 그리고 쿠키 기반으로 된 인증을 해봅시다!
+app.use(auth()); */
+
+/*
+// 404 오류를 먼저 잡아봅시다!
+app.use(notFound());
+// 나머지 오류를 잡죠!
+app.use(otherError()); */
+
+// 리버싱을 지원하자!
+app.enable('trust proxy');
+
+// 기본적인 웹 페이지 title 을 지정하자
+app.locals.title = '김케이인포 - kimkei.info';
+
+// 서버를 실행하자~
+app.listen(process.env.PORT || 8080, function() {
+    /* log.info(pkg.name + " v" + pkg.version + " server listening on %d port!", process.env.PORT || 8080); */
+});
